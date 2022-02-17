a14fdaa9ac54be5418dff3e3c3ca1c1f049b3903
wenying <chenggeng516@163.com>
2022-02-17 18:54:54 +0800

    提交更改

--- StudyOpenGL_02/main.cpp
+++ StudyOpenGL_02/main.cpp
@@ -6,14 +6,28 @@ const int width = 800;
 const int height = 600;
 const char* title = "test gl";
 
+//float vertices[] = {
+//	-0.5f,-0.5,0.0,
+//	0.5f,-0.5,0.0,
+//	0.0f,0.5,0.0,
+//};
+
 float vertices[] = {
-	-0.5f,-0.5,0.0,
-	0.5f,-0.5,0.0,
-	0.0f,0.5,0.0,
+	0.5,0.5,0,
+	0.5,-0.5,0,
+	-0.5,-0.5,0,
+	-0.5,0.5,0
+};
+
+
+unsigned int indices[] = {
+	0,1,3,
+	1,2,3
 };
 
 /*顶点着色器*/
-const char* vertexShaderSource = "#version 330 core\n" 
+const char* vertexShaderSource = 
+"#version 330 core\n" 
 "layout(location = 0) in vec3 aPos;\n" 
 "void main()\n" 
 "{\n" 
@@ -50,18 +64,22 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glViewport(0,0,width,height);
 	glfwSetFramebufferSizeCallback(window,frame_buff_size_callback);
 	//创建并绑定一个顶点缓冲对象
-	unsigned int VBO ,VAO;
+	unsigned int VBO ,VAO,EBO;
 	glGenVertexArrays(1,&VAO);
 	glGenBuffers(1,&VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
 	glEnableVertexAttribArray(0);
@@ -115,12 +133,19 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1,&VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteShader(shaderProgram);
 
 	glfwTerminate();
 
