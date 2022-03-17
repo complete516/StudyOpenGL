ee5446669cba6cf9faaa85958c75b01369bc0f71
wenying <chenggeng516@163.com>
2022-03-17 19:02:47 +0800

    添加创建VAO VBO类

---
+++ Common/MyVertexBuffer.cpp
@@ -0,0 +1,67 @@
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "MyVertexBuffer.h"
+
+MyVertexBuffer::MyVertexBuffer()
+{
+}
+
+void MyVertexBuffer::CreateBindVBO(int size, const void *data)
+{
+
+    unsigned int VAO, VBO;
+
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(VAO);
+    glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+
+    VAOVector.push_back(VAO);
+    VBOVector.push_back(VBO);
+}
+
+void MyVertexBuffer::CreateBindEBO(int size, const void *indices)
+{
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
+
+    EBOVector.push_back(EBO);
+}
+
+void MyVertexBuffer::VertexAttribPointer(int index, int size, int stride, int offset)
+{
+    glVertexAttribPointer(index, size, GL_FLOAT, GL_FALSE, stride, (void *)(sizeof(float) * offset));
+    glEnableVertexAttribArray(index);
+}
+
+void MyVertexBuffer::BindVertexArray()
+{
+    for (auto it = VAOVector.begin(); it != VAOVector.end(); it++)
+    {
+		glBindVertexArray(*it);
+    }
+}
+
+MyVertexBuffer::~MyVertexBuffer()
+{
+    for (auto it = VAOVector.begin(); it != VAOVector.end(); it++)
+    {
+        glDeleteVertexArrays(1, &(*it));
+    }
+
+    for (auto it = VBOVector.begin(); it != VBOVector.end(); it++)
+    {
+        glDeleteBuffers(1, &(*it));
+    }
+
+    for (auto it = EBOVector.begin(); it != EBOVector.end(); it++)
+    {
+        glDeleteBuffers(1, &(*it));
+    }
+}
---
+++ Common/MyVertexBuffer.h
@@ -0,0 +1,31 @@
+#ifndef VERTEXT_BUFFER_H
+#define VERTEXT_BUFFER_H
+#include<iostream>
+#include<vector>
+
+class MyVertexBuffer
+{
+private:
+    /* data */
+    std::vector<unsigned int> VAOVector;
+    std::vector<unsigned int> VBOVector;
+    std::vector<unsigned int> EBOVector;
+
+public:
+    MyVertexBuffer();
+    ~MyVertexBuffer();
+
+    //顶点属性
+    /// index 序号对应顶点着色器中的location
+    /// size 大小
+    /// stride 步长
+    /// offset偏移多少
+    void VertexAttribPointer(int index, int size, int stride, int offset);
+    //绑定顶点数组
+    void BindVertexArray();
+    //创建索引缓冲对象 element buffer object
+    void CreateBindEBO(int size,const void* indices);
+    //创建顶点对象 size 顶点数组大小 data 顶点数组
+    void CreateBindVBO(int size, const void *data);
+};
+#endif //~VERTEXT_BUFFER_H
