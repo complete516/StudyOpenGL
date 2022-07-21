b1bfba4b0ea1accce4c707a5a74d59157c00c717
wenying <chenggeng516@163.com>
2022-07-21 18:32:19 +0800

    1

---
+++ StudyOpenGL_18/StudyOpenGL_18.cpp
@@ -0,0 +1,29 @@
+﻿// StudyOpenGL_18.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#define CONSTDATA
+#define LOADTEXTURE
+
+#include "../Common/MyDefine.h"
+
+#include "../Common/MyWindow.h"
+
+int main()
+{
+    
+    MyWindow mWin("Framebuffers");
+
+    while (!glfwWindowShouldClose(mWin.GetWindow()))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f,0.1f,0.1f,1.0f);
+
+        glfwSwapBuffers(mWin.GetWindow());
+        glfwPollEvents();
+    }
+    
+    std::cout << "Hello World!\n";
+}
+
+
